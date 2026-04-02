fbb450ac7f26326fac194cc0e94bc4e6c5a6e39a
vinicius-mtheiss <viniciusmartinstheiss@gmail.com>
2026-04-02 15:16:08 -0300

    exerciocio02

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         exercicio01();
+        exercicio02();
     }
 
     // 1. Par ou ímpar
@@ -16,6 +17,24 @@ public class Main {
             System.out.println("O número é PAR");
         } else {
             System.out.println("O número é ÍMPAR");
+        }
+    }
+
+    public static void exercicio02() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite o primeiro número: ");
+        int num1 = sc.nextInt();
+
+        System.out.print("Digite o segundo número: ");
+        int num2 = sc.nextInt();
+
+        if (num1 > num2) {
+            System.out.println("O maior número é: " + num1);
+        } else if (num2 > num1) {
+            System.out.println("O maior número é: " + num2);
+        } else {
+            System.out.println("Os números são iguais");
         }
     }
 
