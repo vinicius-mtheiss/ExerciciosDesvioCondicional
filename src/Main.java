3bffde2a37cb5a6f94db61cb82c658f23c257b46
vinicius-mtheiss <viniciusmartinstheiss@gmail.com>
2026-04-02 16:08:05 -0300

    exerciocio13

--- src/Main.java
+++ src/Main.java
@@ -11,9 +11,10 @@ public class Main {
         exercicio07();
         exercicio08();
         exercicio09();
-        exercicio10();*/
+        exercicio10();
         exercicio11();
-        exercicio12();
+        exercicio12();*/
+        exercicio13();
     }
 
     // 1. Par ou ímpar
@@ -236,5 +237,24 @@ public class Main {
             default:
                 System.out.println("Operação inválida");
         }
+    }
+
+    public static void exercicio13() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite o salário: ");
+        double salario = sc.nextDouble();
+
+        double imposto;
+
+        if (salario <= 2000) {
+            imposto = 0;
+        } else if (salario <= 5000) {
+            imposto = salario * 0.10;
+        } else {
+            imposto = salario * 0.20;
+        }
+
+        System.out.println("Imposto a pagar: R$ " + imposto);
     }
 }
